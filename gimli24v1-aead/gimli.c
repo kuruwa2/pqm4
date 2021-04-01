0346b58ad6806bc954b6f6bc4336a1502d26ea28
kuruwa2 <a93019050612@gmail.com>
2021-04-01 10:21:13 +0800

    Add files via upload

--- gimli24v1-aead/gimli.c
+++ gimli24v1-aead/gimli.c
@@ -5,7 +5,10 @@
 //#include "stdio.h"
 
 extern void gimli_asm(uint32_t *state);
-extern void shared_gimli_asm(uint32_t *state, uint32_t *shared_state);
+extern void shared_gimli_asm(uint32_t *state, uint32_t *shared_state, uint32_t *random);
+extern void chacha_random_asm(uint32_t *in, uint32_t *random);
+extern void threshold_gimli_asm(uint32_t *state, uint32_t *shared_state1, uint32_t *shared_state2);
+extern void shared_gimli_asm_opt(uint32_t *state, uint32_t *shared_state);
 
 static uint32_t rotate(uint32_t x,int bits)
 {
@@ -25,27 +28,20 @@ static uint32_t load(uint8_t *state,int pos)
 
 static void store(uint8_t *state,int pos,uint32_t x)
 {
-  state[4*pos+0] = x;// x >>= 8;
+  /*state[4*pos+0] = x;// x >>= 8;
   state[4*pos+1] = x >> 8;// x >>= 8;
   state[4*pos+2] = x >> 16;// x >>= 8;
-  state[4*pos+3] = x >> 24;
+  state[4*pos+3] = x >> 24;*/
+  *((uint32_t *)state + pos) = x;
 }
 
-static void gimli(uint8_t *state)
+/*static void gimli(uint8_t *state)
 {
-  //gimli_asm((uint32_t *) state);
-  //return;
-  //char outstr[128];
   int round, column;
   uint32_t x, y, z;
 
   for (round = 24; round > 0; --round) {
     for (column = 0; column < 4; ++column) {
-      /*asm volatile(
-	"ldr.w r3, [%1, %2]\n\r"
-	"ror %0, r3, #8\n\r"
-        : "=r"(x) : "r"(state) , "r"(column*4) :
-      );*/
       x = rotate(load(state,    column), 24);
       y = rotate(load(state,4 + column),  9);
       z =        load(state,8 + column);
@@ -62,7 +58,6 @@ static void gimli(uint8_t *state)
       x = load(state,2);
       store(state,2,load(state,3));
       store(state,3,x);
-      store(state,0,load(state,0) ^ (0x9e377900 | round));
     }
     else if ((round & 3) == 2) { // big swap: pattern ..S...S...S. etc.
       x = load(state,0);
@@ -73,11 +68,85 @@ static void gimli(uint8_t *state)
       store(state,3,x);
     }
 
+    if ((round & 3) == 0) // add constant: pattern c...c...c... etc.
+      store(state,0,load(state,0) ^ (0x9e377900 | round));
+  }
+}*/
+
+static void threshold_gimli(uint8_t *state, uint8_t *shared_state1, uint8_t *shared_state2)
+{
+  int round, column;
+  uint32_t sx0, sy0, sz0, sx1, sy1, sz1, sx2, sy2, sz2;
+
+  for (round = 24; round > 0; --round) {
+    for (column = 0; column < 4; ++column) {
+      sx0 = rotate(load(state,    column), 24);
+      sy0 = rotate(load(state,4 + column),  9);
+      sz0 =        load(state,8 + column);
+      sx1 = rotate(load(shared_state1,    column), 24);
+      sy1 = rotate(load(shared_state1,4 + column),  9);
+      sz1 =        load(shared_state1,8 + column);
+      sx2 = rotate(load(shared_state2,    column), 24);
+      sy2 = rotate(load(shared_state2,4 + column),  9);
+      sz2 =        load(shared_state2,8 + column);
+
+      store(state,8 + column,sx1 ^ (sz1 << 1) ^ (((sy1 & sz1) ^ (sy1 & sz2) ^ (sy2 & sz1)) << 2));
+      store(state,4 + column,sy1 ^ sx1        ^ (((sx1 & sz1) ^ (sx1 &~sz2) ^ (~sx2& sz1)) << 1));
+      store(state,    column,sz1 ^ sy1        ^ (((sx1 & sy1) ^ (sx1 & sy2) ^ (sx2 & sy1)) << 3));
+      store(shared_state1,8 + column,sx2 ^ (sz2 << 1) ^ (((sy2 & sz2) ^ (sy2 & sz0) ^ (sy0 & sz2)) << 2));
+      store(shared_state1,4 + column,sy2 ^ sx2        ^ (((sx2 | sz2) ^ (~sx2& sz0) ^ (sx0 &~sz2)) << 1));
+      store(shared_state1,    column,sz2 ^ sy2        ^ (((sx2 & sy2) ^ (sx2 & sy0) ^ (sx0 & sy2)) << 3));
+      store(shared_state2,8 + column,sx0 ^ (sz0 << 1) ^ (((sy0 & sz0) ^ (sy0 & sz1) ^ (sy1 & sz0)) << 2));
+      store(shared_state2,4 + column,sy0 ^ sx0        ^ (((sx0 & sz0) ^ (sx0 & sz1) ^ (sx1 & sz0)) << 1));
+      store(shared_state2,    column,sz0 ^ sy0        ^ (((sx0 & sy0) ^ (sx0 & sy1) ^ (sx1 & sy0)) << 3));
+    }
+
+    if ((round & 3) == 0) { // small swap: pattern s...s...s... etc.
+      sx0 = load(state,0);
+      sx1 = load(shared_state1, 0);
+      sx2 = load(shared_state2, 0);
+      store(state,        0,load(state,1) ^ (0x9e377900 | round));
+      store(shared_state1,0,load(shared_state1,1));
+      store(shared_state2,0,load(shared_state2,1));
+      store(state,        1,sx0);
+      store(shared_state1,1,sx1);
+      store(shared_state2,1,sx2);
+      sx0 = load(state,2);
+      sx1 = load(shared_state1, 2);
+      sx2 = load(shared_state2, 2);
+      store(state,        2,load(state,3));
+      store(shared_state1,2,load(shared_state1,3));
+      store(shared_state2,2,load(shared_state2,3));
+      store(state,        3,sx0);
+      store(shared_state1,3,sx1);
+      store(shared_state2,3,sx2);
+    }
+    else if ((round & 3) == 2) { // big swap: pattern ..S...S...S. etc.
+      sx0 = load(state,0);
+      sx1 = load(shared_state1, 0);
+      sx2 = load(shared_state2, 0);
+      store(state,        0,load(state,2));
+      store(shared_state1,0,load(shared_state1,2));
+      store(shared_state2,0,load(shared_state2,2));
+      store(state,        2,sx0);
+      store(shared_state1,2,sx1);
+      store(shared_state2,2,sx2);
+      sx0 = load(state,1);
+      sx1 = load(shared_state1, 1);
+      sx2 = load(shared_state2, 1);
+      store(state,        1,load(state,3));
+      store(shared_state1,1,load(shared_state1,3));
+      store(shared_state2,1,load(shared_state2,3));
+      store(state,        3,sx0);
+      store(shared_state1,3,sx1);
+      store(shared_state2,3,sx2);
+    }
+
     // if ((round & 3) == 0) // add constant: pattern c...c...c... etc.
   }
 }
 
-int crypto_aead_encrypt(
+/*int crypto_aead_encrypt(
   unsigned char *c,unsigned long long *clen,
   const unsigned char *m,unsigned long long mlen,
   const unsigned char *ad,unsigned long long adlen,
@@ -124,37 +193,50 @@ int crypto_aead_encrypt(
   for (i = 0;i < 16;++i) c[i] = state[i];
 
   return 0;
-}
+}*/
 
-int crypto_aead_encrypt_shared(
+/*int crypto_aead_encrypt_shared(
   unsigned char *c,unsigned long long *clen,
   const unsigned char *m,unsigned long long mlen,
   const unsigned char *ad,unsigned long long adlen,
   const unsigned char *nsec,
   const unsigned char *npub,
-  const unsigned char *k,
-  const unsigned char *r
+  const unsigned char *k
 )
 {
-  (void) nsec; // ignore warning
+  uint32_t in[16] = {0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  uint32_t random[288]={0};
+  uint32_t r;
+
   uint8_t state[48];
   uint8_t shared_state[48];
   unsigned long long i;
 
   memcpy(state,npub,16);
   memcpy(state+16,k,32);
+  memcpy((uint8_t *)in+16, m, (mlen<32)? mlen:32);
+  
+  in[14] = rng_get_random_blocking();
+  in[15] = rng_get_random_blocking();
 
-  for (i=0; i<48; ++i){
-    state[i] ^= 1;
-    shared_state[i] = 1;
+  for (i=0; i<12; ++i){
+    r = rng_get_random_blocking();
+    store(state, i, load(state, i) ^ r);
+    store(shared_state, i, r);
   }
 
-  shared_gimli_asm(state, shared_state);
+  chacha_random_asm(in, random);
+  shared_gimli_asm(state, shared_state, random);
   *clen = mlen + 16;
+
+  //char outstr[128];
+  //sprintf(outstr, "%8x\t%8x\t%8x\t%8x\t%8x\t%8x\t%8x\t%8x\r", random[0], random[1],random[2],random[3],random[4],random[5],random[6],random[7]);
+  //hal_send_str(outstr);
 
   while (adlen >= 16) {
     for (i = 0;i < 16;++i) state[i] ^= ad[i];
-    shared_gimli_asm(state, shared_state);
+    chacha_random_asm(in, random);
+    shared_gimli_asm(state, shared_state, random);
     ad += 16;
     adlen -= 16;
   }
@@ -162,14 +244,16 @@ int crypto_aead_encrypt_shared(
   for (i = 0;i < adlen;++i) state[i] ^= ad[i];
   state[adlen] ^= 1;
   state[47] ^= 1;
-  shared_gimli_asm(state, shared_state);
+  chacha_random_asm(in, random);
+  shared_gimli_asm(state, shared_state, random);
 
   while (mlen >= 16) {
     for (i = 0;i < 16;++i) {
       state[i] ^= m[i];
       c[i] = state[i] ^ shared_state[i];
     }
-    shared_gimli_asm(state, shared_state);
+    chacha_random_asm(in, random);
+    shared_gimli_asm(state, shared_state, random);
     c += 16;
     m += 16;
     mlen -= 16;
@@ -182,7 +266,143 @@ int crypto_aead_encrypt_shared(
   c += mlen;
   state[mlen] ^= 1;
   state[47] ^= 1;
-  shared_gimli_asm(state, shared_state);
+  chacha_random_asm(in, random);
+  shared_gimli_asm(state, shared_state, random);
+
+  for (i = 0;i < 16;++i) c[i] = state[i] ^ shared_state[i];
+
+  return 0;
+}*/
+
+int crypto_aead_encrypt_shared(
+  unsigned char *c,unsigned long long *clen,
+  const unsigned char *m,unsigned long long mlen,
+  const unsigned char *ad,unsigned long long adlen,
+  const unsigned char *nsec,
+  const unsigned char *npub,
+  const unsigned char *k
+)
+{
+  (void) nsec; // ignore warning
+  uint8_t state[48];
+  uint8_t shared_state1[48];
+  uint8_t shared_state2[48];
+  uint32_t r1, r2;
+  unsigned long long i;
+
+  memcpy(state,npub,16);
+  memcpy(state+16,k,32);
+
+  for (i=0; i<12; ++i){
+    r1 = rng_get_random_blocking();
+    r2 = rng_get_random_blocking();
+    store(state, i, load(state, i) ^ r1 ^ r2);
+    store(shared_state1, i, r1);
+    store(shared_state2, i, r2);
+  }
+
+  threshold_gimli_asm(state, shared_state1, shared_state2);
+  *clen = mlen + 16;
+
+  while (adlen >= 16) {
+    for (i = 0;i < 16;++i) state[i] ^= ad[i];
+    threshold_gimli_asm(state, shared_state1, shared_state2);
+    ad += 16;
+    adlen -= 16;
+  }
+
+  for (i = 0;i < adlen;++i) state[i] ^= ad[i];
+  state[adlen] ^= 1;
+  state[47] ^= 1;
+  threshold_gimli_asm(state, shared_state1, shared_state2);
+
+  while (mlen >= 16) {
+    for (i = 0;i < 16;++i) {
+      state[i] ^= m[i];
+      c[i] = state[i] ^ shared_state1[i] ^ shared_state2[i];
+    }
+    threshold_gimli_asm(state, shared_state1, shared_state2);
+    c += 16;
+    m += 16;
+    mlen -= 16;
+  }
+
+  for (i = 0;i < mlen;++i) {
+    state[i] ^= m[i];
+    c[i] = state[i] ^ shared_state1[i] ^ shared_state2[i];
+  }
+  c += mlen;
+  state[mlen] ^= 1;
+  state[47] ^= 1;
+  threshold_gimli_asm(state, shared_state1, shared_state2);
+
+  for (i = 0;i < 16;++i) c[i] = state[i] ^ shared_state1[i] ^ shared_state2[i];
+
+  return 0;
+}
+
+int crypto_aead_encrypt_shared_opt(
+  unsigned char *c,unsigned long long *clen,
+  const unsigned char *m,unsigned long long mlen,
+  const unsigned char *ad,unsigned long long adlen,
+  const unsigned char *nsec,
+  const unsigned char *npub,
+  const unsigned char *k
+)
+{
+  uint32_t r;
+
+  uint8_t state[48];
+  uint8_t shared_state[48];
+  unsigned long long i;
+
+  memcpy(state,npub,16);
+  memcpy(state+16,k,32);
+
+  for (i=0; i<12; ++i){
+    r = rng_get_random_blocking();
+    store(state, i, load(state, i) ^ r);
+    store(shared_state, i, r);
+  }
+
+  /*char outstr[128];
+  sprintf(outstr, "%8x\t%8x\t%8x\t%8x\t%8x\t%8x\t%8x\t%8x\r", state[0],state[1],state[2],state[3],state[4],state[5],state[6],state[7]);
+  hal_send_str(outstr);*/
+
+  shared_gimli_asm_opt(state, shared_state);
+  *clen = mlen + 16;
+
+  while (adlen >= 16) {
+    for (i = 0;i < 16;++i) state[i] ^= ad[i];
+    shared_gimli_asm_opt(state, shared_state);
+    ad += 16;
+    adlen -= 16;
+  }
+
+  for (i = 0;i < adlen;++i) state[i] ^= ad[i];
+  state[adlen] ^= 1;
+  state[47] ^= 1;
+  shared_gimli_asm_opt(state, shared_state);
+
+  while (mlen >= 16) {
+    for (i = 0;i < 16;++i) {
+      state[i] ^= m[i];
+      c[i] = state[i] ^ shared_state[i];
+    }
+    shared_gimli_asm_opt(state, shared_state);
+    c += 16;
+    m += 16;
+    mlen -= 16;
+  }
+
+  for (i = 0;i < mlen;++i) {
+    state[i] ^= m[i];
+    c[i] = state[i] ^ shared_state[i];
+  }
+  c += mlen;
+  state[mlen] ^= 1;
+  state[47] ^= 1;
+  shared_gimli_asm_opt(state, shared_state);
 
   for (i = 0;i < 16;++i) c[i] = state[i] ^ shared_state[i];
 
